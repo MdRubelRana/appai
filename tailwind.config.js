02286e98661aeda1fabf0ba61ddf4314863c634f
Md Rubel Rana <rubel.rana@northsouth.edu>
2022-07-23 23:21:09 +0600

    post content added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors:{
+        gray: '#757575',
+        grayLight: '#4c5462',
+        grayDark: '#2b323f',
+        red: '#fd1d1d',
+        yellow: '#fcb045',
+      },
       fontFamily:{
         roboto: ['Roboto', 'sans-serif'],
       },
